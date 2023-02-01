2f50cda665058404c96fe152f8be92b95a2f4723
John West <contactjohnwest@gmail.com>
2023-02-01 11:41:36 -0800

    change font/navbar size

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,40 @@
 <template>
-	<div>
-		<router-view />
-	</div>
+  <div>
+    <router-view />
+  </div>
 </template>
 
-<script lang='ts'>
-export default {
-};
+<script lang="ts">
+export default {};
 </script>
 
-<style>
+<style scoped>
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	padding: 0;
-	margin: 0;
-	overflow-x: hidden;
-	overflow-y: hidden;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding: 0;
+  margin: 0;
+  overflow-x: hidden;
+  overflow-y: hidden;
 }
 
 h1 {
-	color: #000000;
+  color: #000000;
 }
 
 #nav {
-	padding: 30px;
+  padding: 30px;
 }
 
 #nav a {
-	font-weight: bold;
-	color: #2c3e50;
+  font-weight: bold;
+  color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-	color: #42b983;
+  color: #42b983;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,36 +1,47 @@
 <!-- Navbar at the top of the screen -->
 <template>
-	<div>
-		<b-navbar id="nav" toggleable="lg" type="dark">
-			<!-- Display NGCP logo -->
-			<b-img style="width: 75px; padding-left: 15px;" src="@/assets/ngcp.png"></b-img>
-			<div class="nav-title">Ground Control Station</div>
-			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-			<b-navbar-nav class="ml-auto">
-				<b-collapse id="nav-collapse" is-nav>
-					<!-- Display the links to other pages -->
-					<b-navbar-nav>
-						<b-nav-item :active="$route.name == 'Main'">
-							<b-link to="/gcs/main">Main</b-link>
-						</b-nav-item>
-						<b-nav-item :active="$route.name == 'MAC'">
-							<b-link to="/gcs/mac">MAC</b-link>
-						</b-nav-item>
-						<b-nav-item :active="$route.name == 'ERU'">
-							<b-link to="/gcs/eru">ERU</b-link>
-						</b-nav-item>
-						<b-nav-item :active="$route.name == 'MEA'">
-							<b-link to="/gcs/mea">MEA</b-link>
-						</b-nav-item>
-						<!-- <b-nav-item :active="$route.name == 'Form'">
+  <div>
+    <b-navbar id="nav" toggleable="lg" type="dark">
+      <!-- Display NGCP logo -->
+      <!-- <b-img
+        style="width: 75px; padding-left: 15px"
+        src="@/assets/ngcp.png"
+      ></b-img> -->
+      <!-- <div class="nav-title">Ground Control Station</div> -->
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-nav class="ml-auto">
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Display the links to other pages -->
+          <b-navbar-nav id="main-navbar">
+            <b-nav-item :active="$route.name == 'Main'">
+              <b-link to="/gcs/main">
+                <span class="nav-item-text">Main</span>
+              </b-link>
+            </b-nav-item>
+            <b-nav-item :active="$route.name == 'MAC'">
+              <b-link to="/gcs/mac">
+                <span class="nav-item-text">MAC</span>
+              </b-link>
+            </b-nav-item>
+            <b-nav-item :active="$route.name == 'ERU'">
+              <b-link to="/gcs/eru">
+                <span class="nav-item-text">ERU</span>
+              </b-link>
+            </b-nav-item>
+            <b-nav-item :active="$route.name == 'MEA'">
+              <b-link to="/gcs/mea">
+                <span class="nav-item-text">MEA</span>
+              </b-link>
+            </b-nav-item>
+            <!-- <b-nav-item :active="$route.name == 'Form'">
               <b-link to="/form">Form</b-link>
             </b-nav-item> -->
-					</b-navbar-nav>
-				</b-collapse>
-			</b-navbar-nav>
-		</b-navbar>
-		<router-view></router-view>
-	</div>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar-nav>
+    </b-navbar>
+    <router-view></router-view>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
@@ -39,34 +50,44 @@ export default defineComponent({});
 </script>
 <style scoped>
 #nav {
-	background-color: #011949;
-	padding: 8px;
-	width: 100%;
-	height: 8vh;
+  background-color: #011949;
+  padding: 8px;
+  width: 55%;
+  height: 8vh;
+  /* height: 103px; */
 } /* set the background color and size */
 
-#nav a {
-	padding: 5px 20px;
-	text-decoration: none;
-	color: rgb(226, 226, 226);
-	font-size: 25pt;
-	font-weight: 400;
-} /* space the links apart and set the text's color and size */
+.nav-item-text {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 300;
+  font-size: 25px;
+  line-height: 39px;
+  /* identical to box height */
 
-#nav a.router-link-exact-active {
-	color: #61a3ff;
-	text-decoration: underline;
-} /* change color to blue when on that page */
+  text-align: center;
 
-#nav a:hover {
-	color: rgb(144, 165, 209);
-} /* change color to grey when hovering the link */
+  color: #ffffff !important;
 
-.nav-title {
-	font-size: 18pt;
-	padding-left: 25px;
-	font-weight: 400;
-	/* margin: auto; */
-	color: rgb(226, 226, 226);
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
+
+#main-navbar li {
+  padding: 5px 20px;
+  color: rgb(226, 226, 226);
+  font-size: 25pt;
+  font-weight: 200;
+}
+/* space the links apart and set the text's color and size */
+
+#main-navbar a {
+  background-color: #bdc3cb !important;
+
+  text-decoration: none !important;
+}
+
+/* change color to blue when on that page  */
+#nav a:hover {
+  color: rgb(144, 165, 209);
+} /* change color to grey when hovering the link */
 </style>
