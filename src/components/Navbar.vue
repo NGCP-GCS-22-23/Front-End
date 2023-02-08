35dc000fa52bcbede7cfff7df8c54ca4e50b1e3a
John West <contactjohnwest@gmail.com>
2023-02-08 10:57:28 -0800

    change styling and navbar layout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,17 +2,11 @@
 <template>
   <div>
     <b-navbar id="nav" toggleable="lg" type="dark">
-      <!-- Display NGCP logo -->
-      <!-- <b-img
-        style="width: 75px; padding-left: 15px"
-        src="@/assets/ngcp.png"
-      ></b-img> -->
-      <!-- <div class="nav-title">Ground Control Station</div> -->
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-navbar-nav class="ml-auto">
         <b-collapse id="nav-collapse" is-nav>
           <!-- Display the links to other pages -->
-          <b-navbar-nav id="main-navbar">
+          <b-navbar-nav>
             <b-nav-item :active="$route.name == 'Main'">
               <b-link to="/gcs/main">
                 <span class="nav-item-text">Main</span>
@@ -48,46 +42,43 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({});
 </script>
+
 <style scoped>
 #nav {
   background-color: #011949;
   padding: 8px;
   width: 55%;
+  /* width: 650px; */
   height: 8vh;
-  /* height: 103px; */
-} /* set the background color and size */
+}
 
 .nav-item-text {
   font-family: 'Inter';
   font-style: normal;
   font-weight: 300;
-  font-size: 25px;
+  font-size: 20px;
   line-height: 39px;
-  /* identical to box height */
 
   text-align: center;
 
   color: #ffffff !important;
+  padding: 8px 20px;
 
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 
-#main-navbar li {
-  padding: 5px 20px;
-  color: rgb(226, 226, 226);
-  font-size: 25pt;
-  font-weight: 200;
+.nav-item-text:hover {
+  background: #595866;
 }
-/* space the links apart and set the text's color and size */
 
-#main-navbar a {
-  background-color: #bdc3cb !important;
-
+/deep/ a {
   text-decoration: none !important;
 }
 
-/* change color to blue when on that page  */
-#nav a:hover {
-  color: rgb(144, 165, 209);
-} /* change color to grey when hovering the link */
+li {
+  padding: 0px 10px;
+  margin: 15px 9px 10px 9px;
+  color: rgb(226, 226, 226);
+  font-weight: 200;
+}
 </style>
