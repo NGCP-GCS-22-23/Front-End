ad59dd19daea8317aa4b2bbfff335406732d24c2
John West <contactjohnwest@gmail.com>
2023-02-19 14:05:51 -0800

    Fix collapsible menu and appearing/reappearing navbar when cursor close

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,52 +1,85 @@
-<!-- Navbar at the top of the screen -->
 <template>
-  <div>
-    <b-navbar id="nav" toggleable="lg" type="dark">
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <!-- Display the links to other pages -->
-        <b-navbar-nav fill class="w-100">
-          <b-nav-item :active="$route.name == 'Main'">
-            <b-link to="/gcs/main">
-              <span class="nav-item-text">Main</span>
-            </b-link>
-          </b-nav-item>
-          <b-nav-item :active="$route.name == 'MAC'">
-            <b-link to="/gcs/mac">
-              <span class="nav-item-text">MAC</span>
-            </b-link>
-          </b-nav-item>
-          <b-nav-item :active="$route.name == 'ERU'">
-            <b-link to="/gcs/eru">
-              <span class="nav-item-text">ERU</span>
-            </b-link>
-          </b-nav-item>
-          <b-nav-item :active="$route.name == 'MEA'">
-            <b-link to="/gcs/mea" class="change-background">
-              <span class="nav-item-text">MEA</span>
-            </b-link>
-          </b-nav-item>
-          <!-- <b-nav-item :active="$route.name == 'Form'">
-              <b-link to="/form">Form</b-link>
-            </b-nav-item> -->
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-    <router-view></router-view>
-  </div>
+  <div
+    id="custom-div"
+    @mouseleave="showNavbar = false"
+    @mouseenter="showNavbar = true"
+  ></div>
+  <!-- If there is no fixed="top" in <b-navbar> there will be a white space where navbar should be-->
+  <!-- Also add v-show="showNavbar" in <b-navbar> if wanting to work with disappearing navbar-->
+  <b-navbar
+    id="nav"
+    toggleable="lg"
+    type="dark"
+    v-if="showNavbar"
+    fixed="top"
+    @mouseleave="showNavbar = false"
+    @mouseenter="showNavbar = true"
+  >
+    <b-navbar-toggle
+      target="nav-collapse"
+      class="navbar-dark"
+    ></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav fill class="w-100">
+        <b-nav-item :active="$route.name == 'Main'">
+          <b-link to="/gcs/main">
+            <span class="nav-item-text">Main</span>
+          </b-link>
+        </b-nav-item>
+        <b-nav-item :active="$route.name == 'MAC'">
+          <b-link to="/gcs/mac">
+            <span class="nav-item-text">MAC</span>
+          </b-link>
+        </b-nav-item>
+        <b-nav-item :active="$route.name == 'ERU'">
+          <b-link to="/gcs/eru">
+            <span class="nav-item-text">ERU</span>
+          </b-link>
+        </b-nav-item>
+        <b-nav-item :active="$route.name == 'MEA'">
+          <b-link to="/gcs/mea" class="change-background">
+            <span class="nav-item-text">MEA</span>
+          </b-link>
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  <router-view></router-view>
 </template>
+
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+export default defineComponent({
+  data() {
+    return {
+      showNavbar: false,
+    };
+  },
+});
 </script>
 
 <style scoped>
+#custom-div {
+  width: 50%;
+  background-color: transparent !important;
+  height: 130px;
+  top: 0;
+  position: absolute;
+  z-index: 5;
+}
+
+:deep(.nav-fill .nav-item .nav-link) {
+  background-color: #011949;
+  cursor: default;
+}
+
 #nav {
   background-color: #011949;
   padding: 8px;
   width: 50%;
   height: 59px;
+  z-index: 5;
 }
 
 .nav-item-text {
@@ -75,11 +108,7 @@ a.router-link-active > .nav-item-text {
   text-decoration: none !important;
 }
 
-:deep(.nav-fill .nav-item .nav-link, .nav-justified .nav-item .nav-link) {
-  cursor: default;
-}
-
-ul {
-  margin: 10px 9px 10px -20px;
+.navbar-collapse {
+  z-index: 1 !important;
 }
 </style>
