76d3bc87a5174742cf3151f1e28b74f40b033891
kltang6328 <kaylatang02@gmail.com>
2023-05-23 20:47:06 -0700

    updated center coordinates

--- src/helpers/coordinates.js
+++ src/helpers/coordinates.js
@@ -1,8 +1,8 @@
-export let defaultLat = 33.933729
-export let defaultLng = -117.6318437
+export let defaultLat = 33.932//33.933729
+export let defaultLng = -117.63//-117.6318437
 
-export let centerLat = 33.93364332758927
-export let centerLng = -117.62873233754273
+export let centerLat = 33.932//33.93364332758927
+export let centerLng = -117.63//-117.62873233754273
 
 export let defaultPolygon = [
 	{ lat: 33.933729, lng: -117.6318437 }, // marker1
